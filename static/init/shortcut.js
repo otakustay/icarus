06f86332d1b27c388401de604b979af50202633a
otakustay <otakustay@gmail.com>
2015-12-06 14:48:39 +0800

    微调帮助界面

--- static/init/shortcut.js
+++ static/init/shortcut.js
@@ -15,11 +15,7 @@ module.exports = browsingContext => {
 
     surface.registerKeyboardShortcut('F', '全屏/退出全屏', require('../command/fullscreen'));
 
-    surface.registerKeyboardShortcut('N', '下一部漫画', require('../command/nextArchive'));
-    surface.registerKeyboardShortcut('E', '下一部漫画', require('../command/nextArchive'));
-
-    surface.registerKeyboardShortcut('P', '上一部漫画', require('../command/previousArchive'));
-    surface.registerKeyboardShortcut('Q', '上一部漫画', require('../command/previousArchive'));
+    surface.registerKeyboardShortcut(' ', '打开/关闭打扰模式', () => surface.invokeWidget('disturb', 'toggle'));
 
     surface.registerKeyboardShortcut('J', '往后阅读', require('../command/nextStep'));
     surface.registerKeyboardShortcut('S', '往后阅读', require('../command/nextStep'));
@@ -33,11 +29,15 @@ module.exports = browsingContext => {
     surface.registerKeyboardShortcut('H', '上一页', require('../command/previousImage'));
     surface.registerKeyboardShortcut('A', '上一页', require('../command/previousImage'));
 
+    surface.registerKeyboardShortcut('N', '下一部漫画', require('../command/nextArchive'));
+    surface.registerKeyboardShortcut('E', '下一部漫画', require('../command/nextArchive'));
+
+    surface.registerKeyboardShortcut('P', '上一部漫画', require('../command/previousArchive'));
+    surface.registerKeyboardShortcut('Q', '上一部漫画', require('../command/previousArchive'));
+
     surface.registerKeyboardShortcut('R', '恢复上次阅读图片', () => browsingContext.restore());
 
-    surface.registerKeyboardShortcut('I', '显示/隐藏文件名', () => surface.invokeWidget('info', 'toggle'));
+    surface.registerKeyboardShortcut('I', '显示/隐藏文件名（全屏状态有效）', () => surface.invokeWidget('info', 'toggle'));
 
     surface.registerKeyboardShortcut('¿', '显示/隐藏帮助', () => surface.invokeWidget('help', 'toggle'));
-
-    surface.registerKeyboardShortcut(' ', '打开/关闭打扰模式', () => surface.invokeWidget('disturb', 'toggle'));
 };
