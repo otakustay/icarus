5d51a26de9ce28bf283234e9566519741f543ad0
otakustay <otakustay@gmail.com>
2015-12-08 14:08:23 +0800

    从压缩文件中自动除去__MACOSX目录
    Fixed #3

--- service/util/unpack.js
+++ service/util/unpack.js
@@ -10,6 +10,7 @@ let readFile = denodeify(require('fs').readFile);
 let path = require('path');
 
 const IMAGE_EXTENSIONS = new Set(['.jpg', '.jpeg', '.png', '.gif', '.bmp']);
+const BLACKLIST = ['__MACOSX'];
 
 /**
  * 解压压缩文件
@@ -21,6 +22,7 @@ module.exports = async (file) => {
     let fileData = await readFile(file);
     let archiveContent = require('node-zip')(fileData);
     let imageList = Object.keys(archiveContent.files)
+        .filter(file => !BLACKLIST.some(word => file.includes(word)))
         .filter(file => IMAGE_EXTENSIONS.has(path.extname(file)))
         .sort((x, y) => x.toLowerCase().localeCompare(y.toLowerCase()))
         .map(name => archiveContent.files[name]);
