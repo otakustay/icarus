a078d1098118da259db77facaa6dcc59743c41cf
otakustay <otakustay@gmail.com>
2016-01-18 15:48:26 +0800

    实现自己的rar解压逻辑以解决编码问题
    Fix #7

--- service/util/RarArchive.js
+++ service/util/RarArchive.js
@@ -7,11 +7,26 @@
 
 let path = require('path');
 let denodeify = require('denodeify');
-let consume = denodeify(require('all-stream'));
-let RarFile = require('rarfile').RarFile;
+let exec = denodeify(require('child_process').exec);
 let Archive = require('./Archive');
 
 const RAR_TOOL_PATH = path.join(__dirname, '..', '..', '3rd', 'unrar-os-x');
+// 在使用`electron-packager`打包后的应用程序启动时`LANG`环境变量是错误的`C`（即`ASCII`编码），会导致带中文的文件名读取全部错误
+const ENV = Object.assign({}, process.env, {LANG: require('electron').app.getLocale().replace(/-/g, '_')});
+const POSSIBLE_MAX_IMAGE_SIZE = 30 * 1024 * 1024; // 30MB
+
+let loadRarNames = async file => {
+    let command = `${RAR_TOOL_PATH} lb "${file}"`;
+    let output = await exec(command, {env: ENV, encoding: 'utf8'});
+    let names = output.split(/\r?\n/).map(name => ({entryName: name, name: path.basename(name)}));
+    return names;
+};
+
+let readRarContentFile = (file, entryName) => {
+    let command = `${RAR_TOOL_PATH} p -y -idq "${file}" "${entryName}"`;
+    let result = exec(command, {env: ENV, encoding: 'buffer', maxBuffer: POSSIBLE_MAX_IMAGE_SIZE});
+    return result;
+};
 
 /**
  * Rar压缩包封装
@@ -21,22 +36,20 @@ module.exports = class RarArchive extends Archive {
     /**
      * 构造函数
      *
-     * @param {ZipEntry[]} rarFile 由`rarfile`生成的实例
+     * @param {string} file 压缩文件路径
+     * @param {string[]} entries 压缩包中的文件列表
      */
-    constructor(rarFile) {
+    constructor(file, entries) {
         super();
-        this.rarFile = rarFile;
-        this.entries = rarFile.names.map(n => ({entryName: n, name: path.basename(n)}));
+        this.file = file;
+        this.entries = entries;
     }
 
     /**
      * @override
      */
-    async readEntry({entryName}) {
-        let stream = this.rarFile.readStream(entryName);
-
-        let content = await consume(stream);
-        return new Buffer(content, 'binary');
+    readEntry({entryName}) {
+        return readRarContentFile(this.file, entryName);
     }
 
     /**
@@ -45,8 +58,8 @@ module.exports = class RarArchive extends Archive {
      * @param {string} file 文件路径
      * @return {service.util.ZipArchive} 压缩包对象
      */
-    static create(file) {
-        let rarFile = new RarFile(file, {rarTool: RAR_TOOL_PATH});
-        return new Promise(resolve => rarFile.on('ready', () => resolve(new RarArchive(rarFile))));
+    static async create(file) {
+        let entries = await loadRarNames(file);
+        return new RarArchive(file, entries);
     }
 };
