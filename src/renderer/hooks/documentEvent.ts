9096c089a68fb90b6f3e7d65f2b641c089b797ee
otakustay <otakustay@gmail.com>
2020-02-07 12:52:25 +0800

    refractor: 优化useDocumentEvent

--- src/renderer/hooks/documentEvent.ts
+++ src/renderer/hooks/documentEvent.ts
@@ -1,13 +1,15 @@
-import {useLayoutEffect} from 'react';
+import {useRef, useLayoutEffect} from 'react';
 
 export default function useDocumentEvent<K extends keyof DocumentEventMap>(
     eventName: K,
-    handler: (this: Document, ev: DocumentEventMap[K]) => any
+    fn: (ev: DocumentEventMap[K]) => any
 ) {
+    const handler = useRef(fn);
     useLayoutEffect(
         () => {
-            document.addEventListener(eventName, handler);
-            return () => document.removeEventListener(eventName, handler);
+            const trigger = (e: DocumentEventMap[K]) => handler.current(e);
+            document.addEventListener(eventName, trigger);
+            return () => document.removeEventListener(eventName, trigger);
         },
         [eventName, handler]
     );
