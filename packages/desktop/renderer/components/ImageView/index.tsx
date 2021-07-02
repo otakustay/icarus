40c59a47f71eada73d0512e9b302c6137de20049
otakustay <otakustay@gmail.com>
2021-07-02 18:57:36 +0800

    fix: 图片损坏时可以通过上一步、下一步的快捷键切换图片

--- packages/desktop/renderer/components/ImageView/index.tsx
+++ packages/desktop/renderer/components/ImageView/index.tsx
@@ -3,7 +3,7 @@ import {IoImageOutline} from 'react-icons/io5';
 import {Direction} from '@icarus/shared';
 import {useElementSize} from '@huse/element-size';
 import {useGlobalShortcut} from '@/hooks/shortcut';
-import {KEY_NEXT_IMAGE, KEY_PREVIOUS_IMAGE} from '@/dicts/keyboard';
+import {KEY_NEXT_IMAGE, KEY_NEXT_STEP, KEY_PREVIOUS_IMAGE, KEY_PREVIOUS_STEP} from '@/dicts/keyboard';
 import {useLayoutType, useReadingBook, useReadingImageUnsafe} from '@/components/ReadingContextProvider';
 import FullSizeWarn from '@/components/FullSizeWarn';
 import Content from './Content';
@@ -17,6 +17,13 @@ interface Props {
     onNavigate: (direction: Direction) => void;
 }
 
+function ImageBroken({onNavigate}: Props) {
+    useGlobalShortcut(KEY_NEXT_STEP, () => onNavigate('forward'));
+    useGlobalShortcut(KEY_PREVIOUS_STEP, () => onNavigate('backward'));
+
+    return <FullSizeWarn size={100} icon={<IoImageOutline />} description="当前图片已损坏" />;
+}
+
 export default function ImageView({onNavigate}: Props) {
     const book = useReadingBook();
     const image = useReadingImageUnsafe();
@@ -26,8 +33,7 @@ export default function ImageView({onNavigate}: Props) {
     useGlobalShortcut(KEY_PREVIOUS_IMAGE, () => onNavigate('backward'));
 
     if (!image) {
-        // TODO: 图片损坏时把前一下、下一步注册到图片移动上去
-        return <FullSizeWarn size={100} icon={<IoImageOutline />} description="当前图片已损坏" />;
+        return <ImageBroken onNavigate={onNavigate} />;
     }
 
     return (
